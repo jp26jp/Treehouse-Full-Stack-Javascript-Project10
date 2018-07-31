074137e30348cebd10583ee3d79b1f9e2db60b54
John R Perry <john@j-26.com>
2018-07-30 22:28:46 -0600

    Added new book and update functionality

--- models/book.js
+++ models/book.js
@@ -1,17 +1,13 @@
 "use strict"
 module.exports = (sequelize, DataTypes) => {
-    var books = sequelize.define("books", {
-        id             : {
-            type      : DataTypes.INTEGER,
-            primaryKey: true,
-        },
+    var Book = sequelize.define("Book", {
         title          : DataTypes.STRING,
         author         : DataTypes.STRING,
         genre          : DataTypes.STRING,
         first_published: DataTypes.INTEGER,
     }, {})
-    books.associate = function (models) {
+    Book.associate = function (models) {
         // associations can be defined here
     }
-    return books
+    return Book
 }
